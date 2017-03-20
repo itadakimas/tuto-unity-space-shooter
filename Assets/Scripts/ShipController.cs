81f05b5524214badc83c25f38dc4d2153f68fd88
Saïd AHEMT <said.ahemt@nolaroads.com>
2017-03-20 12:39:51 +0100

    creates ShipController

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+	Rigidbody rb;
+
+	void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float horizontalAxis = Input.GetAxis ("Horizontal");
+
+		moveLeft ();
+	}
+
+	private void moveLeft()
+	{
+
+	}
+}
