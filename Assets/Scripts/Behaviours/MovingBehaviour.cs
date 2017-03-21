8c3197d9ec6955f87335ddb4d99c8b5e090a2c69
Saïd AHEMT <said.ahemt@nolaroads.com>
2017-03-21 15:44:28 +0100

    adds MovingBehaviour script to the standard projectile

---
+++ Assets/Scripts/Behaviours/MovingBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MovingBehaviour : MonoBehaviour
+{
+	[SerializeField]
+	int _speed;
+
+	Rigidbody _rb;
+
+	void Start ()
+	{
+		_rb = gameObject.GetComponent<Rigidbody> ();
+		_rb.velocity = new Vector3 (0, 0, 1) * _speed;
+	}
+}
+
+[CustomEditor(typeof(MovingBehaviour))]
+public class MovingBehaviourEditor : Editor
+{
+	SerializedProperty _speedField;
+
+	void OnEnable()
+	{
+		_speedField = serializedObject.FindProperty ("_speed");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update ();
+
+		EditorGUILayout.IntSlider (_speedField, 0, 30, new GUIContent("Speed"));
+
+		serializedObject.ApplyModifiedProperties ();
+	}
+}
