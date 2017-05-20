259e6c2a4866d5a1edbf4d81b2e2a472cec87a8f
Itadakimas <said.ahemt@gmail.com>
2017-05-20 15:29:57 +0200

    reorganizes UI prefabs

---
+++ Assets/HUDController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class HUDController : MonoBehaviour
+{
+  [SerializeField] private GameObject _healthBar;
+  [SerializeField] private GameObject _score;
+
+  private GameObject _healthBarInstance;
+  private GameObject _scoreInstance;
+
+  void Start ()
+  {
+    _healthBarInstance = Instantiate(_healthBar);
+    _healthBarInstance.transform.SetParent(gameObject.transform);
+    _scoreInstance = Instantiate(_score);
+    _scoreInstance.transform.SetParent(gameObject.transform);
+  }
+}
