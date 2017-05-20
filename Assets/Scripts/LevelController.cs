6c90fc71f0e7b753616bbb7c9edb874394aab3ff
Itadakimas <said.ahemt@gmail.com>
2017-05-20 20:20:21 +0200

    reorganizes lights prefabs

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+  [SerializeField] private GameObject _background;
+  [SerializeField] private List<GameObject> _lights;
+
+  private GameObject _backgroundInstance;
+  private List<GameObject> _lightsInstances;
+
+  void Start ()
+  {
+    _backgroundInstance = Instantiate(_background);
+    _backgroundInstance.transform.SetParent(transform);
+
+    _lightsInstances = new List<GameObject>();
+    foreach (GameObject light in _lights)
+    {
+      GameObject lightInstance = Instantiate(light);
+
+      lightInstance.transform.SetParent(transform);
+      _lightsInstances.Add(lightInstance);
+    }
+  }
+}
